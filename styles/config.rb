368c5d2f078d769f052d934740ba75d9ff39a3d3
nvembar <nvembar@yahoo.com>
2015-12-30 11:47:42 -0500

    Fixed static files

--- styles/config.rb
+++ styles/config.rb
@@ -3,10 +3,10 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "../beers/static/css"
+css_dir = "../hundred_beers/static/css"
 sass_dir = "sass"
-images_dir = "../beers/static/images"
-javascripts_dir = "../beers/static/js"
+images_dir = "../hundred_beers/static/images"
+javascripts_dir = "../hundred_beers/static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
