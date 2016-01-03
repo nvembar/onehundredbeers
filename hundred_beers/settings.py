bea1a415b4c259a2cf4de3d983771bc0050c2fbe
nvembar <nvembar@yahoo.com>
2016-01-03 16:40:54 -0500

    Updated logging settings

--- hundred_beers/settings.py
+++ hundred_beers/settings.py
@@ -124,3 +124,32 @@ STATICFILES_DIRS = (
 )
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 LOGIN_REDIRECT_URL = '/beers/'
+
+import os
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+	'formatters': {
+		'simple': {
+			'format': '[%(levelname)s] [%(asctime)s] "%(message)s"'
+		},
+	},
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+			'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+		'beers': {
+			'handlers': ['console'],
+			'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+			'propogate': True
+		},
+    },
+}
