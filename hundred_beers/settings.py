638c8e5a81da570856ba1598f350470591591a46
nvembar <navin.vembar@gmail.com>
2017-06-22 20:19:21 -0400

    Moved SECRET_KEY to environment variable

--- hundred_beers/settings.py
+++ hundred_beers/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xt^8&=li&8s!n84gggs$@x90oep(guy#7yc_%vw@24jloieeur'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', default='0') is '1'
