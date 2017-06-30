e0c723a6803bf8e5ca433ccb17aeeaaf8d176698
nvembar <navin.vembar@gmail.com>
2017-06-30 14:43:03 -0400

    Fixed linting issues

--- beers/base_views.py
+++ beers/base_views.py
@@ -15,43 +15,46 @@ from .forms.contests import ContestForm
 logger = logging.getLogger(__name__)
 
 def index(request):
-    contests = Contest.objects.order_by('created_on')[:5]
+    """Renders the index page with all the contest info"""
+    all_contests = Contest.objects.order_by('created_on')[:5]
     if is_authenticated_user_contest_runner(request):
-        player = Player.objects.get(user_id=request.user.id)
-        for c in contests:
+        player = Player.objects.get(user=request.user)
+        for c in all_contests:
             if c.creator.id == player.id:
                 c.is_creator = True
-    context = { 'contests': contests }
+    context = {'contests': all_contests}
     return render(request, 'beers/index.html', context)
 
 def contests(request):
-    contests = Contest.objects.order_by('created_on')
+    """Returns all the contests"""
+    all_contests = Contest.objects.order_by('created_on')
     player = None
     if is_authenticated_user_player(request):
         try:
             player = Player.objects.get(user_id=request.user.id)
             # Add a field which indicates whether the logged in user is
             # already in a contest or can join it
-            for c in contests:
+            for c in all_contests:
                 c.can_add = Contest_Player.objects.filter(
-                        player_id=player.id, contest_id=c.id).count() is 0
+                    player=player, contest=c).count() is 0
         except Player.DoesNotExist:
             pass
-    context = { 'contests': contests,
-                 'allow_add': is_authenticated_user_contest_runner(request),
-                'player': player }
+    context = {'contests': all_contests,
+               'allow_add': is_authenticated_user_contest_runner(request),
+               'player': player}
     return render(request, 'beers/contests.html', context)
 
 def contest(request, contest_id):
-    contest = get_object_or_404(Contest, id=contest_id)
-    context = { 'contest': contest }
+    """Gets the details of a specific contest and presents them to the user"""
+    this_contest = get_object_or_404(Contest, id=contest_id)
+    context = {'contest': this_contest}
     if request.user.is_authenticated:
         try:
             player = Player.objects.get(user_id=request.user.id)
             context['player'] = player
-            if contest.creator.id == player.id:
+            if this_contest.creator.id == player.id:
                 context['is_creator'] = True
-        except:
+        except Player.DoesNotExist:
             pass
     return render(request, 'beers/contest.html', context)
 
@@ -72,43 +75,47 @@ def contest_add(request):
         if f.is_valid():
             data = f.clean()
             creator = Player.objects.get(user_id=request.user.id)
-            contest = Contest.objects.create_contest(name=data['name'],
-                        creator=creator,
-                        start_date=data['start_date'],
-                        end_date=data['end_date'])
-            contest.save()
-            context = { 'contest': contest }
+            new_contest = Contest.objects.create_contest(name=data['name'],
+                                                         creator=creator,
+                                                         start_date=data['start_date'],
+                                                         end_date=data['end_date'])
+            new_contest.save()
+            context = {'contest': new_contest}
             return render(request, 'beers/contest-add-success.html', context)
     else:
         f = ContestForm()
-    return render(request, 'beers/contest-add.html', { 'form': f })
+    return render(request, 'beers/contest-add.html', {'form': f})
 
 def contest_player(request, contest_id, username):
     """Shows the validated checkins for a player for a given contest"""
-    contest_player = get_object_or_404(Contest_Player.objects.select_related(),
-        contest_id=contest_id, user_name=username)
+    cp = get_object_or_404(Contest_Player.objects.select_related(),
+                           contest_id=contest_id,
+                           user_name=username)
     player_checkins = Contest_Checkin.objects.filter(
-        contest_player_id=contest_player.id).select_related().order_by('-checkin_time')
-    context = { 'player': contest_player, 'checkins': player_checkins }
+        contest_player=cp).select_related().order_by('-checkin_time')
+    context = {'player': cp, 'checkins': player_checkins}
     return render(request, 'beers/contest-player.html', context)
 
 def contest_beers(request, contest_id):
-    contest = get_object_or_404(Contest, id=contest_id)
-    contest_beers = None
-    contest_player = None
-    contest_beers = Contest_Beer.objects.filter(contest_id=contest_id)
-    context = { 'contest': contest, 'contest_beers': contest_beers }
+    """
+    Gets the beers associated with a contest and flags those that have beer
+    had by the user
+    """
+    this_contest = get_object_or_404(Contest, id=contest_id)
+    beers = Contest_Beer.objects.filter(contest=this_contest)
+    context = {'contest': contest, 'contest_beers': beers}
     if request.user.is_authenticated:
         try:
-            contest_player = Contest_Player.objects.get(
-                contest=contest, player__user_id=request.user.id)
-            checkins = Contest_Checkin.objects.filter(contest_player=contest_player)
+            cp = Contest_Player.objects.get(contest=contest,
+                                            player__user_id=request.user.id)
+            checkins = Contest_Checkin.objects.filter(contest_player=cp)
             checkin_ids = [c.contest_beer.id for c in checkins]
-            for b in contest_beers:
+            for b in beers:
                 b.checked_into = b.id in checkin_ids
-            context['contest_player'] = contest_player
+            context['contest_player'] = cp
         except Contest_Player.DoesNotExist:
-            logger.error('Request for user {} for contest {} is not valid'.format(request.user.id, contest))
+            logger.error('Request for user %s for contest %s is not valid',
+                         request.user.username, contest)
     return render(request, 'beers/contest-beers.html', context)
 
 def contest_beer(request, contest_id, beer_id):
@@ -118,19 +125,18 @@ def contest_beer(request, contest_id, beer_id):
 @require_http_methods(['POST'])
 def contest_join(request, contest_id):
     """Add the logged in user to the contest"""
-    contest = get_object_or_404(Contest, id=contest_id)
+    this_contest = get_object_or_404(Contest, id=contest_id)
     if not is_authenticated_user_player(request):
         raise django.core.exceptions.PermissionDenied(
             'User {0} cannot join contests'.format(request.user.username))
     player = Player.objects.get(user_id=request.user.id)
-    try:
-        contest_player = Contest_Player.objects.get(contest_id=contest.id,
-                                                player_id=player.id)
-        context = { 'contest': contest, 'player': player, 'created_new': False }
-    except Contest_Player.DoesNotExist:
-        contest_player = contest.add_player(player)
-        context = { 'contest': contest, 'player': player, 'created_new': True }
+    if Contest_Player.objects.filter(contest=this_contest, player=player).count() != 0:
+        context = {'contest': this_contest, 'player': player, 'created_new': False}
+    else:
+        this_contest.add_player(player)
+        context = {'contest': contest, 'player': player, 'created_new': True}
     return render(request, 'beers/contest-join.html', context)
 
 def instructions(request):
+    """Returns the static instructions page"""
     return render(request, 'beers/instructions.html')
