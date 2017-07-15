6326d8b9bc7a0c0dccb0f8b847b37af86b920144
nvembar <navin.vembar@gmail.com>
2017-07-15 16:43:22 -0400

    Renaming to good name, fixing lint errors

--- beers/management/commands/new_contest.py
+++ beers/management/commands/new_contest.py
@@ -1,17 +1,20 @@
+"""Command to create a new contest"""
+
+import io
+import tempfile
 from django.core.management.base import BaseCommand, CommandError
-from django.db.models import Model
 from django.utils import timezone
-from beers.models import Beer, Player, Contest_Checkin, Contest_Beer, Contest, Contest_Player
+from django.conf import settings
+from beers.models import Contest, Player
 from beers.utils.loader import create_contest_from_csv
 from dateutil.parser import parse
-from django.conf import settings
 import boto3
-import os
-import tempfile
-import io
-import json
+
 
 class Command(BaseCommand):
+    """
+    Command to create a new contest from a CSV stored in an S3 bucket.
+    """
 
     help = "Creates a new contest and loads its beer from a CSV"
 
@@ -25,28 +28,33 @@ class Command(BaseCommand):
 
     def handle(self, *args, **opts):
         if Contest.objects.filter(name=opts['name'][0]).count() > 0:
-            raise ValueError("Contest {} already exists".format(opts['name'][0]))
+            raise CommandError(('Contest {} already '
+                                + 'exists').format(opts['name'][0]))
         runner = None
         try:
             runner = Player.objects.get(user__username=opts['runner'][0])
         except Player.DoesNotExist:
-            raise ValueError("No such user {}".format(opts['runner'][0]))
-        start_date = parse(opts['start_date'][0])
-        end_date = parse(opts['end_date'][0])
+            raise CommandError("No such user {}".format(opts['runner'][0]))
+        start_date = timezone.make_aware(parse(opts['start_date'][0]))
+        end_date = timezone.make_aware(parse(opts['end_date'][0]))
 
         sts = boto3.client('sts')
         role_response = sts.assume_role(RoleArn=settings.LOADER_ROLE_ARN,
-                        RoleSessionName='LoadContest',)
-        print(role_response)
+                                        RoleSessionName='LoadContest',)
+        access_key_id = role_response['Credentials']['AccessKeyId']
+        secret_access_key = role_response['Credentials']['SecretAccessKey']
+        session_token = role_response['Credentials']['SessionToken']
         s3 = boto3.resource('s3',
-            aws_access_key_id=role_response['Credentials']['AccessKeyId'],
-            aws_secret_access_key=role_response['Credentials']['SecretAccessKey'],
-            aws_session_token=role_response['Credentials']['SessionToken'],)
+                            aws_access_key_id=access_key_id,
+                            aws_secret_access_key=secret_access_key,
+                            aws_session_token=session_token,)
         with tempfile.TemporaryFile() as stream:
             s3_object = s3.Object(opts['bucket'][0], opts['file'][0])
             s3_object.download_fileobj(stream)
             stream.seek(0)
             text = io.TextIOWrapper(stream)
             create_contest_from_csv(name=opts['name'][0],
-                start_date=start_date, end_date=end_date,
-                runner=runner, stream=text,)
+                                    start_date=start_date,
+                                    end_date=end_date,
+                                    runner=runner,
+                                    stream=text,)
