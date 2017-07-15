72f603e987bddbd1df7fc01935592d0e1c79eebc
nvembar <navin.vembar@gmail.com>
2017-07-15 11:21:06 -0400

    Fixed load_checkins pylint errors

--- beers/management/commands/load_checkins.py
+++ beers/management/commands/load_checkins.py
@@ -8,23 +8,30 @@ from beers.models import Player, Contest
 from beers.utils.checkin import load_player_checkins
 from dateutil.parser import parse as date_parse
 
-logger = logging.getLogger(__name__)
 
 def convert_to_date(arg):
+    "Converts a string to a timezone aware date"
     try:
         after_date = date_parse(arg)
     except ValueError:
-        raise argparse.ArgumentTypeError('Error: load-checkins got a malformed date: {}'.format(arg))
+        raise argparse.ArgumentTypeError(('Error: load-checkins got a '
+                                          + 'malformed date: {}').format(arg))
     return timezone.make_aware(after_date)
 
+
 class Command(BaseCommand):
+    """
+    A command which loads checkins for a player or many players for a
+    given contest.
+    """
 
     help = 'Loads checkins across contests'
 
     def add_arguments(self, parser):
         parser.add_argument('--player', nargs=1, help='Player')
         parser.add_argument('--contest', nargs=1, help='Contest ID', type=int)
-        parser.add_argument('--after-date', nargs=1, help='After date', type=convert_to_date)
+        parser.add_argument('--after-date', nargs=1, help='After date',
+                            type=convert_to_date)
 
     def handle(self, *args, **opts):
         players = Player.objects.all()
@@ -33,14 +40,17 @@ class Command(BaseCommand):
         if 'player' in opts and opts['player']:
             players = players.filter(user__username=opts['player'][0])
             if players.count() == 0:
-                raise CommandError('Error: no such player: {}'.format(opts['player'][0]))
+                raise CommandError(('Error: no such player: '
+                                    + '{}').format(opts['player'][0]))
         if 'contest' in opts and opts['contest']:
             try:
                 contest_id = opts['contest'][0]
                 Contest.objects.get(id=contest_id)
             except Contest.DoesNotExist:
-                raise CommandError('Error: load-checkins got a contest ID that did not exist: {}'.format(contest_id))
+                raise CommandError(('Error: load-checkins got a contest that'
+                                    + ' did not exist: {}').format(contest_id))
         if 'after_date' in opts and opts['after_date']:
             after_date = opts['after_date'][0]
-        for p in players:
-            load_player_checkins(p, from_date=after_date, contest_id=contest_id)
+        for player in players:
+            load_player_checkins(player, from_date=after_date,
+                                 contest_id=contest_id)
