c494739b0f763463182d1c2c0bf32ec7c40dc4b5
nvembar <navin.vembar@gmail.com>
2017-11-13 00:50:48 -0500

    Added LOGIN_URL

--- hundred_beers/settings.py
+++ hundred_beers/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
+LOGIN_URL='login'
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
