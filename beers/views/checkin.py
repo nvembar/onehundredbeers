7c4f86e46f039b2e83e1f8a16606a7115cb5eb91
nvembar <navin.vembar@gmail.com>
2019-05-12 14:35:18 -0400

    Added add checkin url

--- beers/views/checkin.py
+++ beers/views/checkin.py
@@ -27,16 +27,23 @@ def add_unvalidated_checkin(request, contest_id):
                        "for contest '%s' that they do not own",
                        request.user.username, contest.name)
         raise PermissionDenied("User is not allowed to add checkins")
+    contest_player = Contest_Player.objects.get(contest_id=contest.id, 
+                                                player_id=contest.creator.id)
     untappd_url = request.POST.get('untappd_url', None)
     if untappd_url is None:
         return render(request, 'beers/contest.html', 
                       context={ 'errors': ['No URL provided'],
+                                'contest': contest,
+                                'contest_player': contest_player,
+                                'is_creator': True,
                                 'activetab': 'checkins',},
                       status=400)
     if Unvalidated_Checkin.objects.filter(contest_player__contest_id=contest_id, 
                                           untappd_checkin=untappd_url).count() > 0:
         return render(request, 'beers/contest.html', 
                       context={ 'errors': ['Checkin with that URL already exists'],
+                                'contest': contest,
+                                'contest_player': contest_player,
                                 'activetab': 'checkins',},
                       status=400)
     try:
@@ -45,18 +52,25 @@ def add_unvalidated_checkin(request, contest_id):
                                             player__untappd_username=uv.untappd_user)
         uv.contest_player = player
         uv.save()
-        logger.info('Saved unvalidated checkin at url: {}'.untappd_url)
+        logger.info('Saved unvalidated checkin at url: {}'.format(uv.untappd_checkin))
         return render(request, 'beers/contest.html',
-                      context={ 'activetab': 'checkins', 
+                      context={ 'contest': contest,
+                                'contest_player': contest_player,
+                                'is_creator': True,
+                                'activetab': 'checkins', 
                                 'unvalidated_checkin': uv, })
     except UntappdParseException as e:
         return render(request, 'beers/contest.html', 
                       context={ 'errors': ['Error parsing Untappd URL: {}'.format(e)],
+                                'contest': contest,
+                                'contest_player': contest_player,
                                 'activetab': 'checkins',},
                       status=400)
     except Contest_Player.DoesNotExist:
         return render(request, 'beers/contest.html', 
                       context={ 'errors': ["The player associated with that " +
                                            "login doesn't exist or isn't in the contest"],
+                                'contest': contest,
+                                'contest_player': contest_player,
                                 'activetab': 'checkins',},
                       status=400)
