cef3bdaf377499c7c0fb6200386d4084aea25469
nvembar <navin.vembar@gmail.com>
2019-06-01 15:58:21 -0400

    Create models folder to decompose model objects into

---
+++ beers/models/__init__.py
@@ -0,0 +1,9 @@
+from beers.models.models import (
+    Beer, Brewery, Contest, Contest_Beer, Contest_Bonus, Contest_Brewery,
+    Contest_Checkin, Contest_Player, Player, Unvalidated_Checkin
+)
+
+__all__ = [
+    'Beer', 'Brewery', 'Contest', 'Contest_Beer', 'Contest_Bonus', 'Contest_Brewery',
+    'Contest_Checkin', 'Contest_Player', 'Player', 'Unvalidated_Checkin'
+]
